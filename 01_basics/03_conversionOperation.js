1302c1caae65c284baea7224ffdc6ab1b2e6bdf7
Shahaaqib321 <112758295+Shahaaqib321@users.noreply.github.com>
2024-01-02 07:32:02 +0000

    new 02

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,17 @@
+// basic conversion
+// "33"=> 33
+// "33ab"=>NaN
+// true => 1; false => 
+
+
+//let isLoggedIn = 1
+//let booleanIsLoggedIn= Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+// 1=> true; 0=> false
+// ""=> False
+// "SHAH"=> true
+
+
+let someNumber= 18
+let stringNumber = string(someNumber)
+console.log(stringNumber);
