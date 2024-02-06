a951b82f0c94b2cc593d2d39fab92a1bc9e1d953
Shahaaqib321 <112758295+Shahaaqib321@users.noreply.github.com>
2024-02-06 06:51:26 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict" // treat all js code as newer version
+//alert("hello") // we are use node js not browser
+//console.log(3+ 3); // code readability should be high 
+//console.log("romii")
+let name = "romee"
+let age = 18
+let isLoggedIn = false 
+let state = null
+// number +> to power 53
+// bigint 
+// srting => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique 
+
+// object 
+console.log(typeof age );
